06e0d48b184afe3b8d597e24da6725eded4a27d8
Sumit Girnar <sumitgirnar09@gmail.com>
2025-12-27 17:37:53 +0530

    added 2 more quizzes

--- mockers/src/app/auth/auth.config.ts
+++ mockers/src/app/auth/auth.config.ts
@@ -4,6 +4,7 @@ import { OpenIdConfiguration } from 'angular-auth-oidc-client';
 export const authConfig: OpenIdConfiguration = {
   authority: 'https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_8nWZrk7h3',
   redirectUrl: 'https://www.sumitgirnar.xyz/',
+  // redirectUrl: 'http://localhost:4200/',
   clientId: '99k2hi7sg439ftqdp3imf3ru',
   scope: 'email openid phone profile',
   responseType: 'code',
--- mockers/src/app/components/home/mock-test-display/mock-test-display.ts
+++ mockers/src/app/components/home/mock-test-display/mock-test-display.ts
@@ -18,7 +18,8 @@ export class MockTestDisplay implements OnInit {
 
   // Quiz IDs
   pcm_fst1_quizId = '37a81c5d-6362-41e4-aaf3-9d925579f538';
-  pcm_fst2_quizId = '36a81c5d-6362-41e4-aaf3-9d925579f512';
+  pcm_fst2_quizId = '9c4f2e1a-7b6d-4c8a-9a3e-0f5d6a2b8c41';
+  pcm_fst3_quizId = 'd19e7b35-3f0c-4a92-b8d1-6a4c0f5e8b22'; // MHT CET 2025 Shift 2 19th april
 
   // User + Attempt signals
   username!: string;
@@ -30,7 +31,8 @@ export class MockTestDisplay implements OnInit {
   // Quiz Records
   quizRecords: QuizRecord[] = [
     { quizId: this.pcm_fst1_quizId, quizName: 'PCM FULL SYLLABUS TEST 1', quizType: 'free', isProgress: false, attemptNo: 1 },
-    { quizId: this.pcm_fst2_quizId, quizName: 'PCM FULL SYLLABUS TEST 2', quizType: 'free', isProgress: false, attemptNo: 1 }
+    { quizId: this.pcm_fst2_quizId, quizName: 'PCM FULL SYLLABUS TEST 2', quizType: 'free', isProgress: false, attemptNo: 1 },
+    { quizId: this.pcm_fst3_quizId, quizName: 'PCM FULL SYLLABUS TEST 3', quizType: 'free', isProgress: false, attemptNo: 1 }
   ];
 
   private readonly attemptsUrl = 'https://3xwi0sy6xk.execute-api.ap-south-1.amazonaws.com/dev1';
