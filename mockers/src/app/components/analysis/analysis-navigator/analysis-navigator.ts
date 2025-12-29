63e78ddea49ad50f61bf2f440dc466b88dd6fabf
Sumit Girnar <sumitgirnar09@gmail.com>
2025-12-29 17:42:11 +0530

    improved performance analysis

--- mockers/src/app/components/analysis/analysis-navigator/analysis-navigator.ts
+++ mockers/src/app/components/analysis/analysis-navigator/analysis-navigator.ts
@@ -1,6 +1,6 @@
 import { NgClass, NgFor } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { QuestionModel } from '../../../models/question.model';
+import { QuestionData, QuestionModel } from '../../../models/question.model';
 
 @Component({
   selector: 'app-analysis-navigator',
@@ -14,6 +14,8 @@ export class AnalysisNavigator {
   @Input() snapshotMap!: Map<number, QuestionModel>;
   @Input() currentSection: string | undefined;
   @Output() navigateQuestion = new EventEmitter<number>();
+  @Input() questionMap!: Map<number, QuestionData>;  
+  
 
   // Numbers for all questions
   numbers: number[] = Array.from({ length: 150 }, (_, k) => k + 1);
@@ -28,30 +30,47 @@ export class AnalysisNavigator {
     }
   }
 
-  // Determine CSS class based on question status
-  getBoxClass(status: string): string {
-    switch (status) {
-      case 'not-visited': return 'box-1';
-      case 'not-answered': return 'box-2';
-      case 'answered': return 'box-3';
-      case 'marked': return 'box-4';
-      case 'answered-marked': return 'box-5';
-      default: return 'box-1';
-    }
-  }
+
 
   // Get the status of a question by its questionId
-  getStatus(questionId: number): string {
+getStatus(questionId: number): string {
     const question = this.snapshotMap?.get(questionId);
-
-    if (!question) return this.getBoxClass('not-visited');
-    if (question.IsSaved && question.IsVisited && question.IsAnswered) return this.getBoxClass('answered');
-    if (question.IsMarkedForReview && !question.IsAnswered && question.IsVisited) return this.getBoxClass('marked');
-    if (question.IsMarkedForReview && question.IsAnswered) return this.getBoxClass('answered-marked');
-    if (question.IsVisited && !question.IsAnswered && !question.IsMarkedForReview && !question.IsSaved) return this.getBoxClass('not-answered');
-
-    return this.getBoxClass('not-visited');
+    const correctAnswer = this.questionMap?.get(questionId)?.correctAnswer;
+    
+    let classes: string[] = [];
+    
+    // Determine base status class
+    if (!question) {
+      classes.push('box-1'); // not-visited
+    } else {
+      // Base status
+      if (question.IsSaved && question.IsVisited && question.IsAnswered) {
+        classes.push('box-3'); // answered
+      } else if (question.IsMarkedForReview && !question.IsAnswered && question.IsVisited) {
+        classes.push('box-4'); // marked
+      } else if (question.IsMarkedForReview && question.IsAnswered) {
+        classes.push('box-5'); // answered-marked
+      } else if (question.IsVisited && !question.IsAnswered && !question.IsMarkedForReview && !question.IsSaved) {
+        classes.push('box-2'); // not-answered
+      } else {
+        classes.push('box-1'); // not-visited
+      }
+      
+      // Add correctness indicator on top of base status
+      if (question.IsAnswered && question.selectedOption !== undefined && correctAnswer !== undefined) {
+        if (question.selectedOption === correctAnswer) {
+          classes.push('correct-answer');
+        } else {
+          classes.push('incorrect-answer');
+        }
+      } else if (question.IsVisited && !question.IsAnswered) {
+        classes.push('unanswered');
+      }
+    }
+    
+    return classes.join(' ');
   }
+
 
   // Navigate to a question
   navigateTo(questionId: number): void {
